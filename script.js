13870597ee5613ca89db0617960177a99d4cc5e0
Angel Acosta <angel.s.acosta1@gmail.com>
2022-02-05 15:06:53 -0500

    fixed css link

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000; 
 app.listen(port, () => console.log(`listening on port ${port}`));
 
-app.use('styles.css', (req, res, next) => {res.sendFile(path.join(__dirname, 'public', 'styles.css'))});
+app.use('/styles.css', (req, res, next) => {res.sendFile(path.join(__dirname, 'styles.css'))});
 
 const poker = {
     founded: "1829",
